42652b88c2bf7e0d1ff30ef70fb29a990bac9e85
blu133721 <tiganinpungasrl@gmail.com>
2023-12-02 10:13:41 +0200

    Testing for the first time.

---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <swiftly/swiftly.h>
+#include <swiftly/server.h>
+#include <swiftly/database.h>
+#include <swiftly/commands.h>
+#include <swiftly/configuration.h>
+#include <swiftly/logger.h>
+#include <swiftly/timers.h>
+
+Server *server = nullptr;
+PlayerManager *g_playerManager = nullptr;
+Database *db = nullptr;
+Commands *commands = nullptr;
+Configuration *config = nullptr;
+Logger *logger = nullptr;
+Timers *timers = nullptr;
+
+void OnProgramLoad(const char *pluginName, const char *mainFilePath)
+{
+    Swiftly_Setup(pluginName, mainFilePath);
+
+    server = new Server();
+    g_playerManager = new PlayerManager();
+    commands = new Commands(pluginName);
+    config = new Configuration();
+    logger = new Logger(mainFilePath, pluginName);
+    timers = new Timers();
+}
+
+void OnPluginStart()
+{
+}
+
+void Timer()
+{
+    print("There are %02d players on the server.\n", g_playerManager->GetPlayers());
+}
+
+void OnBombPlanted(Player *player, unsigned short site)
+{
+    print("%s planted a bomb.\n", player->GetName());
+    int timerId = timers->RegisterTimer(40000, []() {
+        static int count = 0;
+        print("Timer: %d seconds\n", ++count);
+    });
+    timers->RegisterTimer(40000, [timerId]() {
+        timers->DestroyTimer(timerId);
+    });
+}
+
+
+
+void OnPluginStop()
+{
+    timers->UnregisterTimers();
+}
+
+const char *GetPluginAuthor()
+{
+    return "";
+}
+
+const char *GetPluginVersion()
+{
+    return "1.0.0";
+}
+
+const char *GetPluginName()
+{
+    return "c4timer";
+}
+
+const char *GetPluginWebsite()
+{
+    return "";
+}
